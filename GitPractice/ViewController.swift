4631ad13022191d59f54f2016f5429daebc13ad2
Никита Цыбаев <cibaev.nikita@icloud.com>
2023-04-14 16:37:47 +0500

    first commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Никита Цыбаев on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
